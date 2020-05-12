3426af42f45dd1c633ee375dfaa2b8335703b213
66430 <66430@local>
2020-05-12 12:38:43 +0800

    last train

---
+++ pac.sh
@@ -0,0 +1,2 @@
+pip install torch-1.0.1-cp37-cp37m-manylinux1_x86_64.whl
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple torchvision==0.2.1 pillow==6.1
---
+++ train.sh
@@ -0,0 +1,5 @@
+
+python train.py --note '511first_s3' --arch '511first_s3' 2>&1 | tee 511first_s3.txt
+python train.py --note '511second_s3' --arch '511second_s3' 2>&1 | tee 511second_s3.txt
+# python train.py --note '511second_s2' --arch '511second_s2' 2>&1 | tee 511second_s2.txt
+# python train.py --note '511second_s1' --arch '511second_s1' 2>&1 | tee 511second_s1.txt
